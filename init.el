8d656ca8879a54960e8f65c125f33b1562af93bc
rdotdk <hariharanrangasamy@gmail.com>
2016-11-02 21:23:58 +0530

    removed ggtags

--- init.el
+++ init.el
@@ -58,16 +58,6 @@
   :init (setq company-minimum-prefix-length 2)
   :config (global-company-mode 1))
 
-;; ggtags
-(use-package ggtags
-  :ensure t
-  :diminish ggtags
-  :init
-  (add-hook 'c-mode-common-hook
-	    (lambda ()
-	      (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-		(ggtags-mode 1)))))
-
 ;; c customizations
 (setq indent-tabs-mode nil)
 (setq tab-width 4)
