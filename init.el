c3745931d315aee191a8e75b10650b988ca8408d
rdotdk <hariharanrangasamy@gmail.com>
2016-12-11 23:45:38 +0530

    Added GNUS mail configuration

--- init.el
+++ init.el
@@ -131,4 +131,26 @@
   :commands esup
   :ensure t)
 
+;; gnus
+;; saving mail locally 
+(require 'gnus)
+(setq nnml-directory "~/gmail")
+(setq message-directory "~/gmail")
+;; don't ignore gmail groups
+(setq gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\”]\”[#’()]")
 
+;; getting mail
+(setq gnus-select-method
+      '(nnimap "gmail"
+               (nnimap-address "imap.gmail.com")
+               (nnimap-server-port 993)
+               (nnimap-stream ssl)))
+
+;; sending mail
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "your-name@gmail.com" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      starttls-use-gnutls t)
