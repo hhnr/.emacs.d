eb1409aac1c1dd9790991d6c1a56ffa6a0ef1773
rdotdk <hariharanrangasamy@gmail.com>
2016-12-16 18:41:56 +0530

    diminish paredit mode

--- init.el
+++ init.el
@@ -179,6 +179,7 @@
 ;; paredit mode
 (use-package paredit
   :ensure t
+  :diminish paredit-mode
   :init
   (add-hook 'emacs-lisp-mode-hook       'enable-paredit-mode)
   (add-hook 'lisp-mode-hook             'enable-paredit-mode)
