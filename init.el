d928ba22b5179387b3f1361ad928a3e45b26f139
rdotdk <hariharanrangasamy@gmail.com>
2017-01-02 20:19:44 +0530

    added golden ratio package

--- init.el
+++ init.el
@@ -201,3 +201,9 @@
 ;; improve emacs display performance
 ;; https://www.masteringemacs.org/article/improving-performance-emacs-display-engine
 (setq redisplay-dont-pause t)
+
+(use-package golden-ratio
+  :ensure t
+  :diminish golden-ratio-mode
+  :init
+  (golden-ratio-mode 1))
