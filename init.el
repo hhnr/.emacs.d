707044f07852b98c02cf7fdc1a1e7b8a0e07a8df
rdotdk <hariharanrangasamy@gmail.com>
2017-03-11 22:41:33 +0530

    disable splash screen on emacs start

---
+++ init.el
@@ -0,0 +1,4 @@
+;;; add lisp directory to load path,
+;;; so that we can require files that are inside lisp directory
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require 'init-appearance)
---
+++ lisp/init-appearance.el
@@ -0,0 +1,3 @@
+;;; disable splash screen on emacs start
+(setq inhibit-splash-screen t)
+(provide 'init-appearance)
