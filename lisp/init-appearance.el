abd3594ba0f0cf6dd764e5311a237dce1a92dc1c
rdotdk <hariharanrangasamy@gmail.com>
2017-03-18 10:34:27 +0530

    Added spacemacs theme

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -1,3 +1,11 @@
 ;;; disable splash screen on emacs start
 (setq inhibit-splash-screen t)
+
+;;; spacemacs theme
+;;; https://github.com/nashamri/spacemacs-theme
+(use-package spacemacs-theme
+  :ensure t
+  :init
+  (load-theme 'spacemacs-dark t))
+
 (provide 'init-appearance)
