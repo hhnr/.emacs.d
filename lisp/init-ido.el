2ce24cad0395d2c25aa2b4b0476869c3fb7769b7
rdotdk <hariharanrangasamy@gmail.com>
2017-03-18 13:04:22 +0530

    Added ido related packages

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -3,6 +3,30 @@
 (use-package ido
   :config
   (ido-mode t)
+  (ido-everywhere t)
   :bind ("C-x C-f" . ido-find-file))
+
+;;; ido-ubiquitous
+;;; https://github.com/DarwinAwardWinner/ido-ubiquitous
+(use-package ido-ubiquitous
+  :ensure t
+  :init
+  (ido-ubiquitous-mode 1))
+
+;;; ido-flx
+;;; https://github.com/lewang/flx
+(use-package flx-ido
+  :ensure t
+  :init
+  (flx-ido-mode 1)
+  (setq ido-enable-flex-matching t))
+
+;;; ido-vertical-mode
+;;; https://github.com/creichert/ido-vertical-mode.el
+(use-package ido-vertical-mode
+  :ensure t
+  :init
+  (ido-vertical-mode 1)
+  (setq ido-vertical-define-keys 'C-n-and-C-p-only))
 
 (provide 'init-ido)
