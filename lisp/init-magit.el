f43efbf7b1d47fbe6a0bd3a3f9d7c91c89195475
rdotdk <hariharanrangasamy@gmail.com>
2017-03-18 13:05:31 +0530

    use ido in magit completion

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -2,5 +2,8 @@
 ;;; https://github.com/magit/magit
 (use-package magit
   :ensure t
-  :commands magit-status)
+  :commands magit-status
+  :init
+  (setq magit-completing-read-function 'magit-ido-completing-read))
+
 (provide 'init-magit)
