121f57a8073c600b0bfa11071ebfcb67fb50e890
rdotdk <hariharanrangasamy@gmail.com>
2017-03-21 22:30:11 +0530

    Added helm-descbinds

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -1,3 +1,5 @@
+;; helm
+;; https://github.com/emacs-helm/helm
 (use-package helm
   :ensure t
   :config
@@ -7,5 +9,11 @@
       (helm-mode 1)))
   :bind ("M-x" . helm-M-x)
   ("C-x C-f" . helm-find-files))
+
+;; helm-descbinds
+;; https://github.com/emacs-helm/helm-descbinds
+(use-package helm-descbinds
+  :ensure t
+  :bind ("C-h b" . helm-descbinds))
 
 (provide 'init-helm)
