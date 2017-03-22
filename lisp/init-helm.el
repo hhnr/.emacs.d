72f1d403936d94508cdf84576e0b46e79448c992
rdotdk <hariharanrangasamy@gmail.com>
2017-03-22 11:55:23 +0530

    Limit helm candidates for speed refer http://pragmaticemacs.com/emacs/find-and-open-files-from-anywhere-with-helm-for-files/

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -6,7 +6,12 @@
   (progn
     (use-package helm-config
       :init
-      (helm-mode 1)))
+      (helm-mode 1))
+    ;; http://pragmaticemacs.com/emacs/find-and-open-files-from-anywhere-with-helm-for-files/
+    ;; limit max number of matches displayed for speed
+    (setq helm-candidate-number-limit 100)
+    ;; ignore boring files like .o and .a
+    (setq helm-ff-skip-boring-files t))
   :bind ("M-x" . helm-M-x)
   ("C-x C-f" . helm-find-files))
 
