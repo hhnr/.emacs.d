69c7b24f076ec0319d7f9bff8f6de3f93887600e
rdotdk <hariharanrangasamy@gmail.com>
2017-03-25 19:26:01 +0530

    Misc fixes from http://tuhdo.github.io/helm-intro.html

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -3,18 +3,28 @@
 (use-package helm
   :ensure t
   :config
-  (progn
-    (use-package helm-config
-      :init
-      (helm-mode 1))
+  (use-package helm-config
+    :init
     ;; http://pragmaticemacs.com/emacs/find-and-open-files-from-anywhere-with-helm-for-files/
     ;; limit max number of matches displayed for speed
     (setq helm-candidate-number-limit 100)
     ;; ignore boring files like .o and .a
-    (setq helm-ff-skip-boring-files t))
+    (setq helm-ff-skip-boring-files t)
+    ;; fuzzy matching for helm-M-x
+    (setq helm-M-x-fuzzy-match t)
+    (setq helm-buffers-fuzzy-matching t
+	  helm-recentf-fuzzy-match t)
+    (setq helm-semantic-fuzzy-match t
+	  helm-imenu-fuzzy-match t)
+    (setq helm-locate-fuzzy-match t)
+    (setq helm-apropos-fuzzy-match t)
+    (add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)
+    (helm-mode 1))
   :bind ("M-x" . helm-M-x)
   ("C-x C-f" . helm-find-files)
-  ("C-x b" . helm-mini))
+  ("C-x b" . helm-mini)
+  ("M-y" . helm-show-kill-ring)
+  ("C-c h x" . helm-register))
 
 ;; helm-descbinds
 ;; https://github.com/emacs-helm/helm-descbinds
