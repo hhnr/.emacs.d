c4a9b704f6cb8ba284f61d18facdfe9822e6c2db
rdotdk <hariharanrangasamy@gmail.com>
2017-03-27 08:39:37 +0530

    No need to load electric pair mode

--- lisp/init-electric-pair.el
+++ lisp/init-electric-pair.el
@@ -1,10 +1,9 @@
 ;; electric pair mode
 ;; automatic character pairing
-(use-package electric-pair-mode
-  :init
-  ;; enable pairing for {}
-  (setq electric-pair-pairs '((?\{ . ?\})))
-  (electric-pair-mode))
+
+;; enable pairing for {}
+(setq electric-pair-pairs '((?\{ . ?\})))
+(electric-pair-mode)
 
 (provide 'init-electric-pair)
 
